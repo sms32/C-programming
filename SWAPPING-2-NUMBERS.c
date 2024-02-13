9cd8c9306ac6586bd26b97fa6c6582a9fa4c2821
sms32 <153702953+sms32@users.noreply.github.com>
2024-02-13 11:55:11 +0530

    Update SWAPPING-2-NUMBERS.c

--- SWAPPING-2-NUMBERS.c
+++ SWAPPING-2-NUMBERS.c
@@ -14,17 +14,4 @@ return 0;
 
 
 
-//SWAPPING WITHOUT USING THIRD VARIABLE
-#include<stdio.h>
-int main(void)
-{
-int a, b, temp;
-printf("Enter two numbers a and b ");
-scanf("%d %d", &a, &b);
-printf("\n Before swapping \na = %d\nb = %d\n", a, b);
-a=a+b;
-b=a-b;
-a=a-b 
-printf("\n After swapping \na = %d\nb = %d\n", a, b);
-return 0; 
-}
+
